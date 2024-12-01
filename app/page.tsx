244dd5082f7208df47b81ec1c05ce8de988bf6cb
aryann15 <rajlysm15@gmail.com>
2024-12-01 14:27:52 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import ServiceCards from './components/ServiceCards'
-import AnimationOverlay from './components/AnimationOverlay'
-import LatestWork from './components/LatestWork'
-import LearningSection from './components/LearningSection'
-import Footer from './components/Footer'
-
+import { Header } from "./components/Header";
+import Hero from "./components/Hero";
+import ServiceCards from "./components/ServiceCards";
+import AnimationOverlay from "./components/AnimationOverlay";
+import LatestWork from "./components/LatestWork";
+import LearningSection from "./components/LearningSection";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <main>
         <Hero />
-        <AnimationOverlay />
+        {/* <AnimationOverlay /> */}
         <ServiceCards />
         <LatestWork />
         <LearningSection />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
-
